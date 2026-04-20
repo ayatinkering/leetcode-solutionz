8e77c127b245f2dda99c681528f125426b0220c4
guthlb <ayatirauthan@gmail.com>
2026-04-20 22:22:54 +0000

    Code challenge submission for question: 1 Two Sum

---
+++ 1 Two Sum/1two-sum.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        int[] ans=new int[2];
+        for(int i=0;i<nums.length;i++){
+             if(map.containsKey(nums[i])){
+                ans[0]=i;ans[1]=map.get(nums[i]);
+             }
+             map.put(target-nums[i],i);   
+        }
+        return ans;
+    }
+}
