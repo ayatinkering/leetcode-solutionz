aeccdaf0575f9a4ca96a3b3a853ede151357cfa8
guthlb <ayatirauthan@gmail.com>
2026-04-20 21:34:17 +0000

    Code challenge submission for question: 217 Contains Duplicate

---
+++ 217 Contains Duplicate/217contains-duplicate.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        int n=nums.length;
+        HashSet<Integer> ans = new HashSet<>();
+        for(int i=0;i<n;i++){
+            if(ans.contains(nums[i]))
+                return true;
+            ans.add(nums[i]);
+        }
+        return false;
+    }
+}
