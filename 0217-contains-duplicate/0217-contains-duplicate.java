7a384f47514d9fcce04a833ad1ea5927c8006961
ayati <123970117+ayatinkering@users.noreply.github.com>
2026-06-08 21:25:00 +0530

    Time: 24 ms (22.61%), Space: 94.9 MB (24.22%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -3,10 +3,7 @@ class Solution {
         HashMap<Integer,Integer> map=new HashMap<>();
         for(int i:nums){
             map.put(i,map.getOrDefault(i,0)+1);
-        }
-        for(int i :map.keySet()){
-            if (map.get(i)>1)
-                return true;
+            if(map.get(i)>1) return true;
         }
         return false;
     }
