91fad84180e4ce5b32d462f4e664122e8c1c6b1d
ayati <123970117+ayatinkering@users.noreply.github.com>
2026-06-08 18:49:55 +0530

    Time: 27 ms (98.89%), Space: 60.5 MB (5.54%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,27 +1,59 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        Set<List<Integer>> listans = new HashSet<>();
-        Arrays.sort(nums); 
+        List<List<Integer>> listans = new ArrayList<>();
 
-        for(int i=0;i<nums.length;i++){
-            int l=i+1;int r=nums.length-1;
-            int target=-nums[i];
-            while(l<r){
-                if(nums[l]+nums[r] > target)    
-                    r--;
-                else if (nums[l]+nums[r] < target)
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length; i++) {
+
+            // Skip duplicate starting elements
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            // No solution possible beyond this point
+            if (nums[i] > 0) {
+                break;
+            }
+
+            int l = i + 1;
+            int r = nums.length - 1;
+
+            while (l < r) {
+
+                int sum = nums[i] + nums[l] + nums[r];
+
+                if (sum < 0) {
                     l++;
+                }
+                else if (sum > 0) {
+                    r--;
+                }
                 else {
-                    ArrayList<Integer> ans=new ArrayList<>();
-                    ans.add(nums[i]); 
-                    ans.add(nums[l]); 
+
+                    List<Integer> ans = new ArrayList<>();
+                    ans.add(nums[i]);
+                    ans.add(nums[l]);
                     ans.add(nums[r]);
-                    listans.add(ans); 
-                    l++; r--; 
+
+                    listans.add(ans);
+
+                    l++;
+                    r--;
+
+                    // Skip duplicate left values
+                    while (l < r && nums[l] == nums[l - 1]) {
+                        l++;
+                    }
+
+                    // Skip duplicate right values
+                    while (l < r && nums[r] == nums[r + 1]) {
+                        r--;
+                    }
                 }
             }
-            
         }
-        return new ArrayList<>(listans);
+
+        return listans;
     }
 }
