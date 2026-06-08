8f27187d3d23a97b8ad8bc453306e0a6560640c2
ayati <123970117+ayatinkering@users.noreply.github.com>
2026-06-08 18:53:32 +0530

    Time: 26 ms (99.62%), Space: 60.3 MB (11.65%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -5,55 +5,38 @@ class Solution {
         Arrays.sort(nums);
 
         for (int i = 0; i < nums.length; i++) {
-
-            // Skip duplicate starting elements
-            if (i > 0 && nums[i] == nums[i - 1]) {
+            if (i > 0 && nums[i] == nums[i - 1]) { //kkip duplicate starting elements
                 continue;
             }
-
-            // No solution possible beyond this point
-            if (nums[i] > 0) {
+            if (nums[i] > 0) { //no sol possible w this starting ele onwards as to get sum 0, it should be negative number
                 break;
             }
 
-            int l = i + 1;
-            int r = nums.length - 1;
-
+            int l = i + 1; int r = nums.length - 1;
             while (l < r) {
-
                 int sum = nums[i] + nums[l] + nums[r];
-
-                if (sum < 0) {
+                if (sum < 0) 
                     l++;
-                }
-                else if (sum > 0) {
+                else if (sum > 0) 
                     r--;
-                }
                 else {
-
                     List<Integer> ans = new ArrayList<>();
                     ans.add(nums[i]);
                     ans.add(nums[l]);
                     ans.add(nums[r]);
-
                     listans.add(ans);
+                    l++; r--;
 
-                    l++;
-                    r--;
-
-                    // Skip duplicate left values
                     while (l < r && nums[l] == nums[l - 1]) {
-                        l++;
+                        l++; //skip duplicates on left
                     }
 
-                    // Skip duplicate right values
                     while (l < r && nums[r] == nums[r + 1]) {
-                        r--;
+                        r--;  //skip duplicates on right
                     }
                 }
             }
         }
-
         return listans;
     }
 }
