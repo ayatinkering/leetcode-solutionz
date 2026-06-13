0c45feac6ee600e51ecb1d86aa500a42d456433f
ayati <123970117+ayatinkering@users.noreply.github.com>
2026-06-13 13:00:01 +0530

    Time: 7 ms (36.13%), Space: 47 MB (8.97%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -4,15 +4,15 @@ class Solution {
         int l=0; int r=0; int w=0; int max=0;
         while(r<s.length()){
             if(set.contains(s.charAt(r))){
-                while(set.contains(s.charAt(r))){
+                while(set.contains(s.charAt(r))){ 
                     set.remove(s.charAt(l));
-                    l++;
+                    l++; //keep removing ele 1 by 1 from the left till it doesnt contain 
                 }
-                set.add(s.charAt(r)); 
+                set.add(s.charAt(r));  //add newly unique ele for new window
                 w=r-l+1;
                 if(w>max) max=w;
             }else{
-                set.add(s.charAt(r));
+                set.add(s.charAt(r)); //ADD TO HASHSET, IF new element
                 w=r-l+1;
                 if(w>max) max=w; 
             }
