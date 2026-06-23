c2f838b55fca1cd4f5398f0762747521510bf97f
ayati <123970117+ayatinkering@users.noreply.github.com>
2026-06-23 10:18:57 +0530

    Time: 973 ms (5.46%), Space: 104.9 MB (80.53%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -4,6 +4,9 @@ class Solution {
             for(int i=0;i<temperatures.length-1;i++){
                 int c=1;
                 for(int j=i+1;j<temperatures.length;j++){
+                    if(i>0&&temperatures[i]>temperatures[i-1]&&ans[i-1]==0){
+                        ans[i]=0; break;
+                    }
                     if(i>0&&temperatures[i]==temperatures[i-1] ){
                         if(ans[i-1]==0) {
                             ans[i]=0;
