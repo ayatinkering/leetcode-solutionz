0fdfac866f1eec3c5522bd6788bef0f15b5372f2
ayati <123970117+ayatinkering@users.noreply.github.com>
2026-06-29 13:13:35 +0530

    Time: 0 ms (100%), Space: 43.2 MB (14.86%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -1,9 +1,9 @@
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if(p==null && q==null) return true;
-        if(p==null || q==null) return false;
+        if(p==null && q==null) return true;  //both trees end at same place
+        if(p==null || q==null) return false; //BOTH arent null, diff distributed nodes
         
-        if(p.val!=q.val) return false;   
-        return (isSameTree(p.left,q.left) && isSameTree(p.right,q.right));
+        if(p.val!=q.val) return false;   //if neither are null but values diff
+        return (isSameTree(p.left,q.left) && isSameTree(p.right,q.right)); //if values same
     }
 }
