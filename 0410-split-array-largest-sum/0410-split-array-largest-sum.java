777680d080892f6cb3311cbdc911895d335b92ec
ayati <123970117+ayatinkering@users.noreply.github.com>
2026-06-30 19:34:44 +0530

    Time: 154 ms (4.31%), Space: 44.6 MB (6.85%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.java
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.java
@@ -1,14 +1,11 @@
-class Solution {
-
+class Solution { //DYNAMIC PROGRAMING, REDO !!!!!!!!!!
     Integer[][] dp;
-
     public int splitArray(int[] nums, int k) {
         dp = new Integer[nums.length][k + 1];
         return helper(nums, 0, k);
     }
 
     public int helper(int[] nums, int start, int parts) {
-
         if (start == nums.length) { // all numbers used
             if (parts == 0)
                 return 0;
@@ -26,18 +23,13 @@ class Solution {
         int sum = 0;
 
         for (int end = start; end <= nums.length - parts; end++) {
-
             sum += nums[end]; // current subarray sum
-
             int remainingAnswer = helper(nums, end + 1, parts - 1);
-
             int maxsum = Math.max(sum, remainingAnswer); // largest after this split
-
             ans = Math.min(ans, maxsum); // best among all splits
         }
 
         dp[start][parts] = ans; // save result
-
         return ans;
     }
 }
