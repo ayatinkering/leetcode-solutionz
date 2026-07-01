33d0cc40e98496d9a48c88fe6e179d6f9196f723
ayati <123970117+ayatinkering@users.noreply.github.com>
2026-07-01 09:28:49 +0530

    Time: 3 ms (99.33%), Space: 48 MB (83%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -9,11 +9,12 @@ class Solution {
         for (int i=0;i<2*n;i++) { //traverse twice for CIRCULAR ARRAY
             int curr = nums[i%n];
             if (stk.isEmpty() || curr <= nums[stk.peek()]) {
-                if (i<n) stk.push(i); //only push original indices
+                if (i<n) stk.push(i); //only push original indices, no need to find ans for duplicated indexes
             } else {
                 while (!stk.isEmpty() && curr>nums[stk.peek()]) {
                     ans[stk.pop()] = curr;
-                }if (i<n) stk.push(i); // only push original indices
+                }
+                if (i<n) stk.push(i); // only push original indices
             }
         }
         return ans;
