a322a0541fb162ad8a24a026279db1b7a150ccf3
ayati <123970117+ayatinkering@users.noreply.github.com>
2026-07-01 09:25:49 +0530

    Time: 4 ms (96.49%), Space: 48.3 MB (68.49%) - LeetHub

---
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+
+        int n = nums.length;
+        int[] ans = new int[n];
+        Arrays.fill(ans, -1);
+        Deque<Integer> stk = new ArrayDeque<>();
+
+        for (int i=0;i<2*n;i++) { //traverse twice for CIRCULAR ARRAY
+            int curr = nums[i%n];
+            if (stk.isEmpty() || curr <= nums[stk.peek()]) {
+                if (i<n) stk.push(i); //only push original indices
+            } else {
+                while (!stk.isEmpty() && curr>nums[stk.peek()]) {
+                    ans[stk.pop()] = curr;
+                }if (i<n) stk.push(i); // only push original indices
+            }
+        }
+        return ans;
+    }
+}
