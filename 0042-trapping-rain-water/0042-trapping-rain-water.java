d1f840380efee9371da709f22d6ce467b4d42391
ayati <123970117+ayatinkering@users.noreply.github.com>
2026-07-01 13:24:09 +0530

    Time: 7 ms (4.68%), Space: 48.1 MB (35.49%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -4,25 +4,19 @@ class Solution {
         Stack<Integer> stk = new Stack<>();
 
         for (int i = 0; i < height.length; i++) {
-
             while (!stk.isEmpty() && height[i] > height[stk.peek()]) {
-
+                int rwall = height[i];
                 int base = height[stk.pop()];
 
                 if (stk.isEmpty()) break;
 
                 int lwall = height[stk.peek()];
-                int rwall = height[i];
-
-                int width = i - stk.peek() - 1;
+                int width = i-stk.peek()-1;
                 int h = Math.min(lwall, rwall) - base;
-
                 area += width * h;
             }
-
-            stk.push(i);   // <-- this was missing
+            stk.push(i);   
         }
-
         return area;
     }
 }
