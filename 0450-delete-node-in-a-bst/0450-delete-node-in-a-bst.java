81d473ee925c67867b36ae71bcbc12ac901ec442
ayati <123970117+ayatinkering@users.noreply.github.com>
2026-07-12 12:58:07 +0530

    Time: 0 ms (100%), Space: 47.7 MB (18.2%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
@@ -1,41 +1,36 @@
 class Solution {
     public TreeNode deleteNode(TreeNode root, int key) {
-
-        if (root == null)
-            return null;
-
-        if (key < root.val) {
-            root.left = deleteNode(root.left, key);
+        if(root==null) return null;
+        
+        if(key<root.val) {
+            root.left=deleteNode(root.left,key); //search left to delete
         }
+        else if(key>root.val){
+            root.right=deleteNode(root.right,key);
+            //when found, returns the value of subteee w deleted, so assign that to right subtree
+        }else{
+            //node found
+            //0. if no child, attach nothing
+            if(root.left==null && root.right==null) return null;
 
-        else if (key > root.val) {
-            root.right = deleteNode(root.right, key);
+            //1. 1 child
+            else if(root.left!=null && root.right==null) return root.left;
+            else if(root.left==null && root.right!=null) return root.right;
+
+            //2. both children
+            else{
+            root.val=findInorderSucc(root.right); //replace w INORDER SUCC VALUE
+            root.right=deleteNode(root.right,root.val); //delete inroder succ node
+            }
         }
-
-        else { // found node to delete
-
-            // no left child
-            if (root.left == null)
-                return root.right;
-
-            // no right child
-            if (root.right == null)
-                return root.left;
-
-            // both children exist
-            TreeNode min = findMin(root.right);
-            root.val = min.val;
-            root.right = deleteNode(root.right, min.val);
-        }
-
         return root;
     }
 
-    public TreeNode findMin(TreeNode root) {
-
-        while (root.left != null)
-            root = root.left;
-
-        return root;
-    }
+        public int findInorderSucc(TreeNode root){
+            while(root.left!=null){
+                root=root.left;
+            }
+            return root.val;
+        }
 }
+    
