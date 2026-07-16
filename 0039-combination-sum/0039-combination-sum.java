a11596beb5f9763772e44c5306cfd0b73d710c53
ayati <123970117+ayatinkering@users.noreply.github.com>
2026-07-16 12:45:13 +0530

    Time: 2 ms (96.66%), Space: 45.9 MB (19.13%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,27 +1,26 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> ans = new ArrayList<>();
-        backtrack(candidates, target, 0, new ArrayList<>(), ans);
+        List<Integer> subset=new ArrayList<>();
+        List<List<Integer>> ans=new ArrayList<>();
+        dfs(candidates,0,subset,ans,target);
         return ans;
     }
-
-    private void backtrack(int[] arr, int remain, int idx, List<Integer> path, List<List<Integer>> ans) {
-        // If remaining is 0, we found a valid combination
-        if (remain == 0) {
-            ans.add(new ArrayList<>(path)); // add a copy
+    public void dfs(int[] candidates,int i,List<Integer> subset,List<List<Integer>> ans, int remaining){
+        if(i>=candidates.length){ //all subsets seen, nothing left
             return;
         }
-        // If we used all candidates or remaining negative -> stop
-        if (idx == arr.length || remain < 0) return;
+        if(remaining==0){
+            ans.add(new ArrayList<>(subset)); //IF SUBSET with sum found
+            return;
+        }
 
-        // Option 1: take arr[idx] (we can take it again later, so idx stays the same)
-        path.add(arr[idx]);
-        backtrack(arr, remain - arr[idx], idx, path, ans);
-
-        // Backtrack: remove last added element
-        path.remove(path.size() - 1);
-
-        // Option 2: skip current candidate and move to next
-        backtrack(arr, remain, idx + 1, path, ans);
+        if(candidates[i]<=remaining){ //TAKE current ele
+            subset.add(candidates[i]);
+            dfs(candidates,i,subset,ans,remaining-candidates[i]);
+            subset.remove(subset.size()-1); //backtrack
+        }
+ 
+        dfs(candidates,i+1,subset,ans,remaining);
+        
     }
 }
