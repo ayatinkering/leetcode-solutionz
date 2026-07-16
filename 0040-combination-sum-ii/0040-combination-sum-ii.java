6dc83c0ebb20e5b6ef768dbb5ffa7e2a37080776
ayati <123970117+ayatinkering@users.noreply.github.com>
2026-07-16 13:54:34 +0530

    Time: 6 ms (74.5%), Space: 45.5 MB (20.87%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -3,30 +3,27 @@ public class Solution {
         List<List<Integer>> ans=new ArrayList<>();
         List<Integer> subset=new ArrayList<>();
      
-        Arrays.sort(candidates);
+        Arrays.sort(candidates); //to get duplicates togetehr
         dfs(candidates,0, subset, ans, target);
         return ans;
     }
 
-    public void dfs(int[] candidates,int i,List<Integer> subset, List<List<Integer>> ans, int rem) {
+    public void dfs(int[] nums,int i,List<Integer> subset, List<List<Integer>> ans, int rem) {
         if (rem==0) {
-            ans.add(new ArrayList<>(subset));
+            ans.add(new ArrayList<>(subset)); //combination found
             return;
         }
-        if (i == candidates.length) { //numbers over
-            return;
-        }
+        if (i>=nums.length) return; //numbers over
 
-        if(candidates[i]<=rem){ //TAKE IT
-            subset.add(candidates[i]);
-            dfs(candidates, i + 1, subset, ans, rem-candidates[i]);
+        if(nums[i]<=rem){ //TAKE IT
+            subset.add(nums[i]);
+            dfs(nums, i + 1, subset, ans, rem-nums[i]);
             subset.remove(subset.size() - 1);
         }
         
-        while (i+1<candidates.length && candidates[i] == candidates[i + 1]) {
-            i++;
+        while (i+1<nums.length && nums[i]==nums[i + 1]) {
+            i++; //increments i to ignore duplicates, only then start the NEXT BRANCHES
         }
-
-        dfs(candidates, i + 1, subset, ans, rem); //DONT TAKE
+        dfs(nums, i+1, subset, ans, rem); //DONT TAKE
     }
 }
