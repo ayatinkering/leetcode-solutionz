3864db7b714c5880272e3a9b9c39f77c8823f288
ayati <123970117+ayatinkering@users.noreply.github.com>
2026-07-31 18:56:31 +0530

    Time: 26 ms (47.49%), Space: 44.5 MB (22.46%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.java
+++ 0133-clone-graph/0133-clone-graph.java
@@ -1,5 +1,4 @@
 /*
-Definition for a Node.
 class Node {
     public int val;
     public List<Node> neighbors;
@@ -7,38 +6,30 @@ class Node {
         val = 0;
         neighbors = new ArrayList<Node>();
     }
-    public Node(int _val) {
-        val = _val;
-        neighbors = new ArrayList<Node>();
-    }
-    public Node(int _val, ArrayList<Node> _neighbors) {
-        val = _val;
-        neighbors = _neighbors;
-    }
 }
 */
 
 public class Solution {
     public Node cloneGraph(Node node) {
-        Map<Node, Node> oldToNew = new HashMap<>();
+        Map<Node, Node> map = new HashMap<>();
 
-        return dfs(node, oldToNew);
+        return dfs(node, map);
     }
 
-    private Node dfs(Node node, Map<Node, Node> oldToNew) {
+    public Node dfs(Node node, Map<Node, Node> map) {
         if (node == null) {
             return null;
         }
 
-        if (oldToNew.containsKey(node)) {
-            return oldToNew.get(node);
+        if (map.containsKey(node)) {
+            return map.get(node);
         }
 
         Node copy = new Node(node.val);
-        oldToNew.put(node, copy);
+        map.put(node, copy);
 
         for (Node nei : node.neighbors) {
-            copy.neighbors.add(dfs(nei, oldToNew));
+            copy.neighbors.add(dfs(nei, map));
         }
 
         return copy;
